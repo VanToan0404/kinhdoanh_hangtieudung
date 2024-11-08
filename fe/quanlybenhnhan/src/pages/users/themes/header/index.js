f3802612aacb136e55ed58168c5c4ae251518d00
Văn Toàn <dracaryss2004@gmail.com>
2024-11-08 17:43:18 +0700

    Hoàn thành header__top

--- fe/quanlybenhnhan/src/pages/users/themes/header/index.js
+++ fe/quanlybenhnhan/src/pages/users/themes/header/index.js
@@ -1,8 +1,105 @@
-import React, { memo } from "react"
+import React, { memo, useState } from "react";
+import "./style.scss";
+import { BsFacebook, BsInstagram, BsBezier2, BsFillHouseDoorFill, BsMailbox } from "react-icons/bs";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import { Link } from "react-router-dom";
+import { formater } from '../../../../utils/fomater';
+import { ROUTERS } from '../../../../utils/routers';
+
 const Header = () => {
-    return (<>
-        <h1>Header</h1>
-    </>
+    const [menus, setMEnus] = useState([
+        {
+            name: "Trang chủ",
+            path: ROUTERS.USER.HOME,
+        },
+        {
+            name: "Cửa hàng",
+            path: ROUTERS.USER.PRODUCT,
+        },
+        {
+            name: "Sản phẩm",
+            path: "",
+            inShowSubmenu: false,
+            child: [
+                { name: "Thịt", path: "" },
+                { name: "Rau củ", path: "" },
+                { name: "Thức ăn nhanh", path: "" },
+            ]
+        },
+        {
+            name: "Bài viết",
+            path: "",
+        },
+        {
+            name: "Liên hệ",
+            path: "",
+        },
+    ]);
+
+    return (
+        <>
+            <div className="header__top">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-6 header__top_left">
+                            <ul>
+                                <li><BsMailbox /> hello@gmail.com</li>
+                                <li>miễn phí ship {formater(200000)}</li>
+                            </ul>
+                        </div>
+                        <div className="col-6 header__top_right">
+                            <ul>
+                                <li><Link to=""><BsFacebook /></Link></li>
+                                <li><Link to=""><BsInstagram /></Link></li>
+                                <li><Link to=""><BsBezier2 /></Link></li>
+                                <li><Link to=""><BsFillHouseDoorFill /></Link><span>Đăng nhập</span></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="container">
+                <div className="row">
+                    <div className="col-xl-3">
+                        <div className="header__logo">
+                            <h1>SIVI SHOP</h1>
+                        </div>
+                    </div>
+                    <div className="col-xl-6">
+                        <nav className="header__menu">
+                            <ul>
+                                {
+                                    menus?.map((menu, menuKey) => {
+                                        return (  // Cần trả về giá trị JSX trong map()
+                                            <li key={menuKey} className={menuKey===0?"active":""}>
+                                                <Link to={menu?.path}>{menu?.name}</Link>
+                                            </li>
+                                        );
+                                    })
+                                }
+                                {/* <li>
+                                    <ul>
+                                        <li>Thịt</li>
+                                    </ul>
+                                </li> */}
+                            </ul>
+                        </nav>
+                    </div>
+                    <div className="col-xl-3">
+                        <div className="header__cart">
+                            <div className="header__cart__price">
+                                <span>{formater(111)}</span>
+                            </div>
+                            <ul>
+                                <li>
+                                    <Link to="#"><AiOutlineShoppingCart /><span>5</span></Link>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 };
 
--- fe/quanlybenhnhan/src/utils/routers.js
+++ fe/quanlybenhnhan/src/utils/routers.js
@@ -1,8 +1,7 @@
-export const ROUTERS={
-    USER:{
+export const ROUTERS = {
+    USER: {
         HOME: "",
         PROFILE: "thong-tin-ca-nhan",
-
+        PRODUCT: "/san-pham",
     },
-
-}
+};
