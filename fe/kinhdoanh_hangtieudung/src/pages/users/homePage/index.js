3c569356c838a61fb3be0d948e5650042b15da24
Văn Toàn <dracaryss2004@gmail.com>
2024-11-13 18:12:53 +0700

    Hôm sau làm tiếp

--- fe/kinhdoanh_hangtieudung/src/pages/users/homePage/index.js
+++ fe/kinhdoanh_hangtieudung/src/pages/users/homePage/index.js
@@ -1,11 +1,31 @@
-import React,{ memo } from "react"
-const profilePage = () => {
-    return (<>
+import React from 'react';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 
-    <h1>home page</h1>
+// Đường dẫn hình ảnh của bạn
+import CaltImg from '../../../assets/users/images/categories/cart_1.png';
 
-    </>
-    );
+const MyComponent = () => {
+  // Cấu hình cho Carousel
+  const responsive = {
+    superLargeDesktop: { breakpoint: { max: 4000, min: 3000 }, items: 5 },
+    desktop: { breakpoint: { max: 3000, min: 1024 }, items: 3 },
+    tablet: { breakpoint: { max: 1024, min: 464 }, items: 2 },
+    mobile: { breakpoint: { max: 464, min: 0 }, items: 1 }
+  };
+
+  return (
+    <div className="container container__categories_slider">
+      <Carousel responsive={responsive} className="categories_slider">
+        <div 
+          className="categories_slider_item"
+          style={{ backgroundImage: `url(${CaltImg})` }}
+        >
+          {/* Nội dung của item, nếu có */}
+        </div>
+      </Carousel>
+    </div>
+  );
 };
 
-export default memo(profilePage);
+export default MyComponent;
